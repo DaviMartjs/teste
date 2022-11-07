0054ccd5a11c7abcf6ed0e6b9d8e11f97cf3ea36
DaviMartjs <davimart2005@gmail.com>
2022-11-07 15:49:11 -0300

    Added header and avatar components

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,20 @@
 import './style.css'
 
-function Header() {
+export function Header() {
     return (
         <>
-        
+        <header className='header'>
+                <div id="icon"><img src="./ibridge.png" alt="" /></div>
+                <nav>
+                    <ul className="buttons">
+                        <li>Link</li>
+                        <li>Link</li>
+                        <li>Link</li>
+                  </ul>
+                </nav>
+                <button id="button">Button</button>
+        </header>
         </>
     )
 }
+export default Header
